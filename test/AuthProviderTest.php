04d0966852a074b4edfd8805938d68dc3cd28c0e
Nicola Farina <farina@netbuilder.it>
2023-10-10 14:16:56 +0200

    MF/SPONSOR/ngxuc7g2e | AuthProvider hotfix

--- test/AuthProviderTest.php
+++ test/AuthProviderTest.php
@@ -4,6 +4,9 @@
 namespace multidialogo\client\test;
 
 
+use DateInterval;
+use DateTimeImmutable;
+use DateTimeZone;
 use GuzzleHttp\Client as HttpClient;
 use GuzzleHttp\Handler\MockHandler;
 use GuzzleHttp\HandlerStack;
@@ -15,10 +18,62 @@ use multidialogo\client\Auth\FileTokenStorage;
 use multidialogo\client\Auth\VolatileTokenStorage;
 use multidialogo\client\test\TestUtils\JsonResources;
 use PHPUnit\Framework\TestCase;
+use stdClass;
+use function PHPUnit\Framework\assertEquals;
 
 class AuthProviderTest extends TestCase
 {
-    public function testVolatileCredentialStore()
+    private string $storageFolder = __DIR__ . '/TestUtils/FileTokenStorage';
+
+    private const USER1_OK_RESPONSE = <<<json
+        {
+            "status": "CREATED",
+            "data": {
+                "id": "25ad1beadfeb089cb5bd0ea20da8a610.0004178967cf186762382528d7e994ba081z8111551061",
+                "type": "auth-tokens",
+                "attributes": {
+                    "token": "USER_1_OK_RESPONSE_TOKEN",
+                    "category": "Bearer",
+                    "createdAt": "2022-12-01T11:18:38Z",
+                    "expireAt": "2022-12-01T14:18:38Z",
+                    "refreshToken": "USER_1_OK_RESPONSE_REFRESH_TOKEN",
+                    "refreshTokenExpireAt": "2023-12-16T11:18:38Z"
+                }
+            }
+        }
+json;
+
+    private const USER2_OK_RESPONSE = <<<json
+        {
+            "status": "CREATED",
+            "data": {
+                "id": "25ad1beadfeb089cb5bd0ea20da8a610.0004178967cf186762382528d7e994ba081z8111551061",
+                "type": "auth-tokens",
+                "attributes": {
+                    "token": "USER_2_OK_RESPONSE_TOKEN",
+                    "category": "Bearer",
+                    "createdAt": "2022-12-01T11:18:38Z",
+                    "expireAt": "2022-12-01T14:18:38Z",
+                    "refreshToken": "USER_2_OK_RESPONSE_REFRESH_TOKEN",
+                    "refreshTokenExpireAt": "2022-12-16T11:18:38Z"
+                }
+            }
+        }
+json;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // clean previous test run results
+        foreach (glob($this->storageFolder . '/*') as $file) {
+            if (is_file($file)) {
+                unlink($file);
+            }
+        }
+    }
+
+    public function testVolatileTokenStorageCanStoreCredentials()
     {
         // with
         $storage = new VolatileTokenStorage();
@@ -45,28 +100,22 @@ class AuthProviderTest extends TestCase
 
         // assert
         $this->assertEquals('25ad1beadfeb089cb5bd0ea20da8a610.0004178967cf186762382528d7e994ba081z8111551061', $tokenString);
-        $this->assertEquals('25ad1beadfeb089cb5bd0ea20da8a610.0004178967cf186762382528d7e994ba081z8111551061', $token->getToken());
+        $this->assertEquals($tokenString, $token->getToken());
     }
 
-    public function testShouldStoreCredentials()
+    public function testFileTokenStorageCanStoreCredentials()
     {
-        $storageFolder = __DIR__ . '/TestUtils/FileTokenStorage';
-
-        // clean previous test run results
-        foreach (glob($storageFolder . '/*') as $file) {
-            if (is_file($file)) {
-                unlink($file);
-            }
-        }
-
-        $storage = new FileTokenStorage($storageFolder);
-
         $container = [];
         $history = Middleware::history($container);
 
+        $now = new DateTimeImmutable("now", new DateTimeZone("UTC"));
+        $user1OkResponseObj = $this->setExpireDatesAtDate($now, self::USER1_OK_RESPONSE);
+        $user2OkResponseObj = $this->setExpireDatesAtDate($now, self::USER2_OK_RESPONSE);
+
         $mock = new MockHandler([
-            new Response(201, [], JsonResources::User1Okresponse),
-            new Response(201, [], JsonResources::User2Okresponse),
+            new Response(201, [], json_encode($user1OkResponseObj)),
+            new Response(201, [], json_encode($user2OkResponseObj)),
+            new Response(201, [], json_encode($user2OkResponseObj)),
         ]);
 
         $handlerStack = new HandlerStack($mock);
@@ -80,10 +129,10 @@ class AuthProviderTest extends TestCase
 
         $user1UserName = 'user1@some_domain.com';
         $user1Password = '!@**"';
-        $user2UserName = 'user2@some_OtherStrangeDomain.com';
-        $user2Password = 'bWaRx9';
 
-        // login as 'user1'
+        $storage = new FileTokenStorage($this->storageFolder);
+
+        // Given that we login as 'user1'
         $provider = new AuthProvider($httpClient, $storage, $user1UserName, $user1Password);
 
         $tokenString = $provider->getToken();
@@ -95,30 +144,175 @@ class AuthProviderTest extends TestCase
 
         $storage->read($user1UserName, $token, $refreshToken);
 
-        $this->assertEquals('0869acb92cc97d066745cfac59cb185e.0004178967cf186762382528d7e994ba081z8111551061', $tokenString);
-        $this->assertEquals('0869acb92cc97d066745cfac59cb185e.0004178967cf186762382528d7e994ba081z8111551061', $token->getToken());
+        $this->assertEquals($tokenString, $token->getToken());
+        $this->assertEquals('USER_1_OK_RESPONSE_TOKEN', $tokenString);
 
-        // login as 'user2'
+        // Then we login as 'user2'
+        $user2UserName = 'user2@some_OtherStrangeDomain.com';
+        $user2Password = 'bWaRx9';
+
         $provider = new AuthProvider($httpClient, $storage, $user2UserName, $user2Password);
 
         $tokenString = $provider->getToken();
 
         $storage->read($user2UserName, $token, $refreshToken);
 
-        $this->assertEquals('a418a9118e23c0b2f9146c4c98ce2ec4.0004178967cf186762382528d7e994ba081z8111551061', $tokenString);
-        $this->assertEquals('a418a9118e23c0b2f9146c4c98ce2ec4.0004178967cf186762382528d7e994ba081z8111551061', $token->getToken());
+        $this->assertEquals($tokenString, $tokenString);
+        $this->assertEquals('USER_2_OK_RESPONSE_TOKEN', $token->getToken());
 
-        // login again as user1 and NO password
+        // Then we login again as user1 and NO password
         $provider = new AuthProvider($httpClient, $storage, $user1UserName, null);
 
         $tokenString = $provider->getToken();
 
         $storage->read($user1UserName, $token, $refreshToken);
 
-        $this->assertEquals('0869acb92cc97d066745cfac59cb185e.0004178967cf186762382528d7e994ba081z8111551061', $tokenString);
-        $this->assertEquals('0869acb92cc97d066745cfac59cb185e.0004178967cf186762382528d7e994ba081z8111551061', $token->getToken());
+        $this->assertEquals($tokenString, $token->getToken());
+        $this->assertEquals('USER_1_OK_RESPONSE_TOKEN', $tokenString);
 
-        // The third call should NOT be done, "user1" should be retrieved from file cache
+        // The third call should NOT be done, "user1" token (which is still valid) should be retrieved from file cache
         self::assertCount(2, $container);
     }
+
+    private function setExpireDatesAtDate(DateTimeImmutable $startDate, string $tokenPayload): stdClass
+    {
+        $tokenObj = json_decode($tokenPayload);
+        $tokenObj->data->attributes->createdAt = $startDate->format('Y-m-d\TH:i:s\Z');
+        $expireAt = $startDate->add(new DateInterval("PT3H"));
+        $tokenObj->data->attributes->expireAt = $expireAt->format('Y-m-d\TH:i:s\Z');
+        $refreshExpireAt = $startDate->add(new DateInterval("P15D"));
+        $tokenObj->data->attributes->refreshTokenExpireAt = $refreshExpireAt->format('Y-m-d\TH:i:s\Z');
+
+        return $tokenObj;
+    }
+
+    public function testShouldCallAuthenticateWhenNoTokenAreStored()
+    {
+        $user1UserName = 'user1@some_domain.com';
+        $user1Password = '!@**"';
+
+        $storage = new FileTokenStorage($this->storageFolder);
+
+        // Scenario: NO tokens are stored, authenticate call must be issued
+
+        $container = [];
+        $history = Middleware::history($container);
+
+        $now = new DateTimeImmutable("now", new DateTimeZone("UTC"));
+        $user1OkResponseObj = $this->setExpireDatesAtDate($now, self::USER1_OK_RESPONSE);
+
+        $mock = new MockHandler([
+            new Response(201, [], json_encode($user1OkResponseObj)),
+        ]);
+
+        $handlerStack = new HandlerStack($mock);
+
+        $handlerStack->push($history);
+
+        $httpClient = new HttpClient([
+            'base_uri' => 'http://any.url/any/path',
+            'handler' => $handlerStack
+        ]);
+
+        $provider = new AuthProvider($httpClient, $storage, $user1UserName, $user1Password);
+
+        $actual = $provider->getToken();
+
+        self::assertEquals("USER_1_OK_RESPONSE_TOKEN", $actual);
+
+        // Call to refresh token is needed here
+        self::assertCount(1, $container);
+        assertEquals('/any/users/login', $container[0]['request']->getUri()->getPath());
+    }
+
+    public function testShouldCallRefreshTokenWhenStoredAuthIsExpiredAndRefreshIsValid()
+    {
+        $user1UserName = 'user1@some_domain.com';
+        $user1Password = '!@**"';
+
+        $storage = new FileTokenStorage($this->storageFolder);
+
+        // Scenario: an auth token is stored already, but it's expired. The refresh token must be used
+        $pastMoment = (new DateTimeImmutable("now", new DateTimeZone("UTC")))->sub(new DateInterval("PT240M"));
+        $futureMoment = (new DateTimeImmutable("now", new DateTimeZone("UTC")))->add(new DateInterval("PT240M"));
+        $storage->write($user1UserName,
+            new AuthToken("USER1_START_EXPIRED_AUTH_TOKEN", $pastMoment),
+            new AuthToken("USER1_START_VALID_REFRESH_TOKEN", $futureMoment)
+        );
+
+        $container = [];
+        $history = Middleware::history($container);
+
+        $now = new DateTimeImmutable("now", new DateTimeZone("UTC"));
+
+        $user1OkResponseObj = $this->setExpireDatesAtDate($now, self::USER1_OK_RESPONSE);
+
+        $mock = new MockHandler([
+            new Response(201, [], json_encode($user1OkResponseObj)),
+        ]);
+
+        $handlerStack = new HandlerStack($mock);
+
+        $handlerStack->push($history);
+
+        $httpClient = new HttpClient([
+            'base_uri' => 'http://any.url/any/path',
+            'handler' => $handlerStack
+        ]);
+
+        $provider = new AuthProvider($httpClient, $storage, $user1UserName, $user1Password);
+
+        $actual = $provider->getToken();
+
+        self::assertEquals("USER_1_OK_RESPONSE_TOKEN", $actual);
+
+        // Should have called login/refresh
+        self::assertCount(1, $container);
+        assertEquals('/any/users/login/refresh', $container[0]['request']->getUri()->getPath());
+    }
+
+    public function testShouldCallAuthenticateWhenBothStoredAuthAndRefreshAreExpired()
+    {
+        $user1UserName = 'user1@some_domain.com';
+        $user1Password = '!@**"';
+
+        $storage = new FileTokenStorage($this->storageFolder);
+
+        // Scenario: the stored auth and refresh are both expired
+        $pastMoment = (new DateTimeImmutable("now", new DateTimeZone("UTC")))->sub(new DateInterval("PT240M"));
+        $storage->write($user1UserName,
+            new AuthToken("USER1_START_EXPIRED_AUTH_TOKEN", $pastMoment),
+            new AuthToken("USER1_START_EXPIRED_REFRESH_TOKEN", $pastMoment)
+        );
+
+        $container = [];
+        $history = Middleware::history($container);
+
+        $now = new DateTimeImmutable("now", new DateTimeZone("UTC"));
+        $user1OkResponseObj = $this->setExpireDatesAtDate($now, self::USER1_OK_RESPONSE);
+
+        $mock = new MockHandler([
+            new Response(201, [], json_encode($user1OkResponseObj)),
+        ]);
+
+        $handlerStack = new HandlerStack($mock);
+
+        $handlerStack->push($history);
+
+        $httpClient = new HttpClient([
+            'base_uri' => 'http://any.url/any/path',
+            'handler' => $handlerStack
+        ]);
+
+        $provider = new AuthProvider($httpClient, $storage, $user1UserName, $user1Password);
+
+        $actual = $provider->getToken();
+
+        self::assertEquals("USER_1_OK_RESPONSE_TOKEN", $actual);
+
+        // Call to refresh token is needed here
+        self::assertCount(1, $container);
+        assertEquals('/any/users/login', $container[0]['request']->getUri()->getPath());
+    }
+
 }
